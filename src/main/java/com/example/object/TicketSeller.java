9765257ab89c9a68ce1b5d19931be0157e627ed8
손대현 책임 보험/증권DX3팀 (dh.son@lgcns.com <dh.son@CNS.LGCNS.COM>
2025-06-19 14:34:12 +0900

    캡슐화

--- src/main/java/com/example/object/TicketSeller.java
+++ src/main/java/com/example/object/TicketSeller.java
@@ -1,12 +1,14 @@
 package com.example.object;
 
-import lombok.Getter;
 
-@Getter
 public class TicketSeller {
     private TicketOffice ticketOffice;
 
     public TicketSeller(TicketOffice ticketOffice) {
         this.ticketOffice = ticketOffice;
+    }
+
+    public void sellTo(Audience audience) {
+        ticketOffice.plusAmount(audience.buy(ticketOffice.getTicket()));
     }
 }
