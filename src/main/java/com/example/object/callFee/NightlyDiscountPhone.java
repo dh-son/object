0f2fecb30a6cc4d4c68979b968fda4e73ec8459c
손대현 책임 보험/증권DX3팀 (dh.son@lgcns.com <dh.son@CNS.LGCNS.COM>
2025-07-02 16:56:37 +0900

    duplication

---
+++ src/main/java/com/example/object/callFee/NightlyDiscountPhone.java
@@ -0,0 +1,46 @@
+package com.example.object.callFee;
+
+import com.example.object.movieReservation.Money;
+
+import java.time.Duration;
+import java.util.ArrayList;
+import java.util.List;
+
+public class NightlyDiscountPhone {
+    private static final int LATE_NIGHT_HOUR = 22;
+
+    private Money nightlyAmount;
+    private Money regularAmount;
+    private Duration seconds;
+    private List<Call> calls = new ArrayList<>();
+    private double taxRate;
+
+    public NightlyDiscountPhone(Money nightlyAmount, Money regularAmount, Duration seconds, double taxRate) {
+        this.nightlyAmount = nightlyAmount;
+        this.regularAmount = regularAmount;
+        this.seconds = seconds;
+        this.taxRate = taxRate;
+    }
+
+    public void call(Call call) {
+        calls.add(call);
+    }
+
+    public List<Call> getCalls() {
+        return calls;
+    }
+
+    public Money calculateFee() {
+        Money result = Money.ZERO;
+
+        for (Call call : calls) {
+            if (call.getFrom().getHour() >= LATE_NIGHT_HOUR) {
+                result = result.plus(nightlyAmount.times(call.getDuration().getSeconds() / seconds.getSeconds()));
+            } else {
+                result = result.plus(regularAmount.times(call.getDuration().getSeconds() / seconds.getSeconds()));
+            }
+        }
+
+        return result.plus(result.times(taxRate));
+    }
+}
